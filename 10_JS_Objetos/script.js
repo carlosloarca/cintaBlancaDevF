e1183ed6c444484959d32255897499c7f3b403f1
loarca <carlos.loarca@gmail.com>
2020-04-08 19:02:37 -0500

    clase carlos objetosm

--- 10_JS_Objetos/script.js
+++ 10_JS_Objetos/script.js
@@ -10,37 +10,51 @@ var arreglo = [1, 3, 5, 8];
 // Notation
 
 var objeto = {
-    // Atributos
     nombre: "La noche estrellada",
     autor: "Van Gogh",
-    tecnica: "Óleo sobre un lienzo",
-    colores: ["#FFFFFF", "#000000"],
+    tecnica: "Oleo",
+    colores: ["#FFFFFF"],
     anio: 1889,
-    lugar: "Ámsterdam",
+    lugar: "Amsterdam",
     mensaje: "Es una obra muy linda",
-    museo: "Museo de Van Gogh",
-
-    // Métodos
+    museo: "Museo Van Gogh",
+    url: "https://www.salirconarte.com/wp-content/uploads/2017/06/orig_64571-750x430.jpg",
     calcularEdad: function () {
-        return 2020 - this.anio
+    return 2020 - this.anio
     },
     numeroColores: function () {
-        if (this.colores.length === 1) {
-            return "La pintura " + this.nombre + " tiene " + this.colores.length + " color."
-        } else {
-            return "La pintura " + this.nombre + " tiene " + this.colores.length + " colores."
-        }
+    if (this.colores.length === 1) {
+    return "La pintura " + this.nombre + " del pintor " + this.autor + " tiene " + this.colores.length + " color"
+    } else {
+    return "La pintura " + this.nombre + " del pintor " + this.autor + " tiene " + this.colores.length + " colores"
+    }
     },
     obtenerInfo: function () {
-        return "La obra " + this.nombre + " fue pintada por " + this.autor + " en el año " + this.anio;
+    return "La obra " + this.nombre + " fue pintada por " + this.autor + " en el año " + this.anio
     }
-}
-
-console.log(objeto);
-console.log(objeto.autor);
-
-console.log(objeto.calcularEdad());
-console.log(objeto.numeroColores());
-console.log(objeto.obtenerInfo());
-
-
+    }
+    var objeto01 = {
+    nombre: "El grito",
+    autor: "Edvard Munch",
+    anio: "1999",
+    url: "https://cdn3.m.admexico.mx/uploads/images/thumbs/mx/ad/1/s/2019/33/el_grito_pintura_8782_600x.jpg"
+    }
+    var objeto02 = {
+    nombre: "La noche estrellada",
+    autor: "Van Gogh",
+    tecnica: "Oleo",
+    }
+    var objetoNombre = document.getElementById("objeto-nombre");
+    var objetoMensaje = document.getElementById("objeto-mensaje");
+    var objetoUrl = document.getElementById("objeto-url");
+    var objeto01Nombre = document.getElementById("objeto01-nombre");
+    var objeto01Mensaje = document.getElementById("objeto01-mensaje")
+    var objeto01Url = document.getElementById("objeto01-url");
+    //console.log(objetoNombre);
+    //console.log(objetoMensaje);
+    objetoNombre.innerHTML = objeto.nombre;
+    objetoMensaje.innerHTML = objeto.obtenerInfo();
+    objetoUrl.src = objeto.url;
+    objeto01Nombre.innerHTML = objeto01.nombre;
+    objeto01Mensaje = objeto01.autor;
+    objeto01Url.src = objeto01.url;
