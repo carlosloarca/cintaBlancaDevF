c221b357252177a2921ed1404be8a7c6756db0c0
loarca <carlos.loarca@gmail.com>
2020-04-11 15:55:42 -0500

    mis comments

---
+++ 10CLObjetos/script.js
@@ -0,0 +1,25 @@
+var arreglo = [1, 2, 5, 8];
+//No tiene un orden 
+//Llave: valor
+
+//JSON
+
+//JavaScript
+//Object
+//Notation
+
+
+var objeto = {
+    nombre: "La noche estrellada",
+    autor: "Van Gogh",
+    tecnica: "Óleo sobre tela",
+    colores: ["#FFFFFF", "#000000"],
+    anio: 1889,
+    lugar: "Amsterdam",
+    mensaje: "Es  una obra muy linda",
+    museo: "Esta obra esta en el museo de Van Gogh",
+}
+
+
+
+console.log (objeto);
