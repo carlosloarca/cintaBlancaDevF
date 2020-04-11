a2bd1041bf0d2e6af83c8c2de287a520590648b8
loarca <carlos.loarca@gmail.com>
2020-04-11 17:24:58 -0500

    mis comments2

--- 10CLObjetos/script.js
+++ 10CLObjetos/script.js
@@ -10,16 +10,40 @@ var arreglo = [1, 2, 5, 8];
 
 
 var objeto = {
+    //Atributos
     nombre: "La noche estrellada",
     autor: "Van Gogh",
     tecnica: "Óleo sobre tela",
-    colores: ["#FFFFFF", "#000000"],
+    colores: ["#FFFFFF", ],
     anio: 1889,
     lugar: "Amsterdam",
     mensaje: "Es  una obra muy linda",
     museo: "Esta obra esta en el museo de Van Gogh",
+
+
+    //Métodos
+    calcularEdad: function () {
+        return 2020 - this.anio
+    },
+    numeroColores: function () {
+        if (this.colores.length === 1) {
+
+            return "la pintura " + this.nombre + " tiene " + this.colores.length + " color "
+        } else {
+            return "la pintura " + this.nombre + " tiene " + this.colores.length + " colores "
+        }
+    },
+
+
+    obtenerInfo: function(){
+        return " La obra " + this.nombre + " fue pintada por " + this.autor + " en  el  año " + this.anio
+    },
+
 }
 
+console.log(objeto);
+console.log(objeto.nombre);
 
-
-console.log (objeto);
+console.log(objeto.calcularEdad());
+console.log(objeto.numeroColores());
+console.log(objeto.obtenerInfo());
